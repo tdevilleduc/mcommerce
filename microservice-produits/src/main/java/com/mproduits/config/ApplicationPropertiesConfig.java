747ed22152afef7c6348c9baede9a9ceb57f29d7
Thomas Deville-Duc <devilleduc.thomas@gmail.com>
2020-05-08 17:24:19 +0200

    [feat] récupérer la config du microservice produit via config-server

--- microservice-produits/src/main/java/com/mproduits/config/ApplicationPropertiesConfig.java
+++ microservice-produits/src/main/java/com/mproduits/config/ApplicationPropertiesConfig.java
@@ -1,10 +1,12 @@
 package com.mproduits.config;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Component;
 
 @Component
 @ConfigurationProperties("mes-configs")
+@RefreshScope
 public class ApplicationPropertiesConfig {
 
     private int limitDeProduits;
