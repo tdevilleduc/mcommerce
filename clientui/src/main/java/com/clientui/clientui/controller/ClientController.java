da4d1a6a57ea1a8e28ac21cf5d070a529fc553d4
Thomas Deville-Duc <devilleduc.thomas@gmail.com>
2020-05-08 16:35:46 +0200

    [feat] ajout de la page html afficherUnProduit

--- clientui/src/main/java/com/clientui/clientui/controller/ClientController.java
+++ clientui/src/main/java/com/clientui/clientui/controller/ClientController.java
@@ -5,6 +5,7 @@ import com.clientui.clientui.proxies.MicroserviceProduitsProxy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -23,5 +24,15 @@ public class ClientController {
         model.addAttribute("produits", produits);
 
         return "Accueil";
+    }
+
+    @RequestMapping("/details-produit/{id}")
+    public String ficheProduit(@PathVariable int id, Model model){
+
+        ProductBean produit = mProduitsProxy.recupererUnProduit(id);
+
+        model.addAttribute("produit", produit);
+
+        return "FicheProduit";
     }
 }
