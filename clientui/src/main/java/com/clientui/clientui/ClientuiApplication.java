7941f91ddddb3a301d9ea4f65270d935886cf40f
Thomas Deville-Duc <devilleduc.thomas@gmail.com>
2020-05-08 16:12:27 +0200

    [feat] add feign : clientui recupere la liste des products

--- clientui/src/main/java/com/clientui/clientui/ClientuiApplication.java
+++ clientui/src/main/java/com/clientui/clientui/ClientuiApplication.java
@@ -2,8 +2,10 @@ package com.clientui.clientui;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients("com.clientui")
 public class ClientuiApplication {
 
 	public static void main(String[] args) {
