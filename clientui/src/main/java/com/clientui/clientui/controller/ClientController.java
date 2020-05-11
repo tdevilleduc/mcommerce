3df2539b17921e628a072376ffe5270fe6f6b25d
Thomas Deville-Duc <devilleduc.thomas@gmail.com>
2020-05-11 20:15:18 +0200

    [feat] implementer l'ensemble des connexions microservice

--- clientui/src/main/java/com/clientui/clientui/controller/ClientController.java
+++ clientui/src/main/java/com/clientui/clientui/controller/ClientController.java
@@ -1,20 +1,35 @@
 package com.clientui.clientui.controller;
 
+import com.clientui.clientui.beans.CommandeBean;
+import com.clientui.clientui.beans.PaiementBean;
 import com.clientui.clientui.beans.ProductBean;
+import com.clientui.clientui.proxies.MicroserviceCommandeProxy;
+import com.clientui.clientui.proxies.MicroservicePaiementProxy;
 import com.clientui.clientui.proxies.MicroserviceProduitsProxy;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Date;
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 @Controller
 public class ClientController {
 
     @Autowired
     MicroserviceProduitsProxy mProduitsProxy;
+
+    @Autowired
+    MicroservicePaiementProxy mPaiementProxy;
+
+    @Autowired
+    MicroserviceCommandeProxy mCommandeProxy;
 
     @RequestMapping("/")
     public String accueil(Model model) {
@@ -34,5 +49,46 @@ public class ClientController {
         model.addAttribute("produit", produit);
 
         return "FicheProduit";
+    }
+
+    @RequestMapping("/commander-produit/{produitId}/{montant}")
+    public String commanderProduit(@PathVariable int produitId, @PathVariable Double montant, Model model){
+
+        CommandeBean commande = new CommandeBean();
+        commande.setProductId(produitId);
+        commande.setDateCommande(new Date());
+        commande.setQuantite(1);
+
+        CommandeBean commandeAjoutee = mCommandeProxy.ajouterCommande(commande);
+
+        model.addAttribute("commande", commandeAjoutee);
+        model.addAttribute("montant", montant);
+
+        return "Paiement";
+    }
+
+    @RequestMapping(value = "/payer-commande/{idCommande}/{montantCommande}")
+    public String payerCommande(@PathVariable int idCommande, @PathVariable Double montantCommande, Model model){
+
+        PaiementBean paiementAExcecuter = new PaiementBean();
+
+        paiementAExcecuter.setIdCommande(idCommande);
+        paiementAExcecuter.setMontant(montantCommande);
+        paiementAExcecuter.setNumeroCarte(numcarte());
+
+        ResponseEntity<PaiementBean> paiement = mPaiementProxy.payerUneCommande(paiementAExcecuter);
+
+        Boolean paiementAccepte = Boolean.FALSE;
+        if(paiement.getStatusCode() == HttpStatus.CREATED)
+            paiementAccepte = Boolean.TRUE;
+
+        model.addAttribute("paiementOk", paiementAccepte);
+
+        return "Confirmation";
+    }
+
+    private Long numcarte() {
+
+        return ThreadLocalRandom.current().nextLong(1000000000000000L,9000000000000000L );
     }
 }
--- microservice-paiement/src/main/java/com/mpaiement/web/controller/PaiementController.java
+++ microservice-paiement/src/main/java/com/mpaiement/web/controller/PaiementController.java
@@ -1,13 +1,17 @@
 package com.mpaiement.web.controller;
 
+import com.mpaiement.beans.CommandeBean;
 import com.mpaiement.dao.PaiementDao;
 import com.mpaiement.model.Paiement;
+import com.mpaiement.proxies.MicroserviceCommandeProxy;
 import com.mpaiement.web.exceptions.PaiementExistantException;
 import com.mpaiement.web.exceptions.PaiementImpossibleException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PaiementController {
@@ -15,9 +19,11 @@ public class PaiementController {
     @Autowired
     PaiementDao paiementDao;
 
+    @Autowired
+    MicroserviceCommandeProxy microserviceCommandeProxy;
+
     @PostMapping(value = "/paiement")
     public ResponseEntity<Paiement>  payerUneCommande(@RequestBody Paiement paiement){
-
 
         //Vérifions s'il y a déjà un paiement enregistré pour cette commande
         Paiement paiementExistant = paiementDao.findByidCommande(paiement.getIdCommande());
@@ -26,12 +32,13 @@ public class PaiementController {
         //Enregistrer le paiement
         Paiement nouveauPaiement = paiementDao.save(paiement);
 
-
         if(nouveauPaiement == null) throw new PaiementImpossibleException("Erreur, impossible d'établir le paiement, réessayez plus tard");
 
+        CommandeBean commande = microserviceCommandeProxy.recupererUneCommande(paiement.getIdCommande());
 
+        commande.setCommandePayee(Boolean.TRUE);
 
-        //TODO Nous allons appeler le Microservice Commandes ici pour lui signifier que le paiement est accepté
+        microserviceCommandeProxy.updateCommande(commande);
 
         return new ResponseEntity<Paiement>(nouveauPaiement, HttpStatus.CREATED);
 
